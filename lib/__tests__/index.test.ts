b666f0cbb12a9c675fbfea611540b46615a9d86f
Mayank <mayank.srmu@gmail.com>
2025-04-05 12:06:34 +0530

    Add unit test to assert no console.log
    and remove smoke test as console.log test covers that

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { describe, it } from "vitest";
+import { describe, it, vi } from "vitest";
 import { toDocx } from "@m2d/core"; // Adjust path based on your setup
 import { unified } from "unified";
 import remarkParse from "remark-parse";
@@ -8,12 +8,12 @@ import { imagePlugin } from "../src";
 
 const markdown = fs.readFileSync("../sample.md", "utf-8");
 
-describe("toDocx", () => {
-  it("should handle images", async ({ expect }) => {
-    const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
+describe.concurrent("toDocx", () => {
+  const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
 
-    const docxBlob = await toDocx(mdast, {}, { plugins: [imagePlugin()] });
-
-    expect(docxBlob).toBeInstanceOf(Blob);
+  it("should not have any console.log", async ({ expect }) => {
+    const consoleSpy = vi.spyOn(console, "log");
+    await toDocx(mdast, {}, { plugins: [imagePlugin()] });
+    expect(consoleSpy).not.toHaveBeenCalled();
   });
 });
