53b8602d4168ee70e83f95ce09f40f325d60df21
Mayank <mayank.srmu@gmail.com>
2025-04-08 12:10:00 +0530

    separate test cases to pin point the issue

--- lib/__tests__/index.test.ts
+++ lib/__tests__/index.test.ts
@@ -11,6 +11,11 @@ const markdown = fs.readFileSync("../sample.md", "utf-8");
 describe.concurrent("toDocx", () => {
   const mdast = unified().use(remarkParse).use(remarkGfm).parse(markdown);
 
+  it("should handle images", async ({ expect }) => {
+    const docxBlob = await toDocx(mdast, {}, { plugins: [imagePlugin()] });
+    expect(docxBlob).toBeInstanceOf(Blob);
+  });
+
   it("should not have any console.log", async ({ expect }) => {
     const consoleSpy = vi.spyOn(console, "log");
     await toDocx(mdast, {}, { plugins: [imagePlugin()] });
